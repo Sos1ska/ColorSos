d1f1c28a9d6dc02bd994afb52c5012aa8ff44768
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2022-12-18 18:54:08 +0300

    Update win32.py
    Fixing bug on the platform "Termux"

--- ColorSos/win32.py
+++ ColorSos/win32.py
@@ -1,4 +1,7 @@
 def ANSImode():
-    import ctypes
-    kernel32 = ctypes.windll.kernel32
-    kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+    import ctypes, os
+    if os.sys.platform == "win32":
+        kernel32 = ctypes.windll.kernel32
+        kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+    else:
+        pass
